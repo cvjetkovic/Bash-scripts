a1ef8cafd6f806443d5de58c360db1da169eb9a2
cvjetkovic <58811764+cvjetkovic@users.noreply.github.com>
2021-04-02 12:16:34 +0200

    Added restore command

--- postgres_dump_restore.sh
+++ postgres_dump_restore.sh
@@ -1,8 +1,12 @@
 ### Vladimir Cvjetkovic
-# Postgres dump
+# Postgres dump & restore
 
 now="$(date +'%d.%m.%Y_%T')"
 db_name='database'
 dump_location='/backups/'
 
 pg_dump -U postgres -F p $db_name > $dump_location/$db_name'_'$now.sql
+
+# Restore
+# psql -d $db_name -f $db_name'_'dump.sql 
+
